8fd8c41bd76bbb34ace9994e3421fc264b42f118
BigSmashy <geremy.sorlini.protic@gmail.com>
2015-01-30 15:20:33 -0500

    Header Files
    Default Constructor created, functions and variables added to the
    header.

--- DynamicArray/dynamicArray.h
+++ DynamicArray/dynamicArray.h
@@ -7,10 +7,21 @@ class DynamicArray
 
 public:
 
+	DynamicArray(unsigned int _capacite = 100) :
+		capacite(_capacite)
+		{
+		tabElements = new int[capacite];
+		}
 
+	void setElement(unsigned int _index, int _valeur);
+	int getElement(unsigned int _index) const;
+	unsigned int getCapacite() const;
+	void setCapacite(unsigned int _capacite);
 
 private:
 
+	int *tabElements;
+	unsigned int capacite;
 
 };
 
