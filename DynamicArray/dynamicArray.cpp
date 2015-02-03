e4f7aae803d9be2162d3dff3bda4fdad3724ad69
BigSmashy <geremy.sorlini.protic@gmail.com>
2015-02-03 08:13:52 -0500

    Test 6 réussi

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -27,6 +27,13 @@ void DynamicArray::setCapacite(const unsigned int _capacite){
 }
 
 void DynamicArray::setElement(const unsigned int _index, const int _valeur){
-	if (_index + 1 > capacite) throw std::runtime_error("La pile est trop petite; l'index recherché n'éxiste pas.");
-	tabElements[_index] = _valeur;
+	//if (_index + 1 > capacite) throw std::runtime_error("La pile est trop petite; l'index recherché n'éxiste pas.");
+	if (_index + 1 > capacite){
+		setCapacite(_index + 1);
+		setElement(_index, _valeur);
+	}
+	else{
+		tabElements[_index] = _valeur;
+	}
+	
 }
